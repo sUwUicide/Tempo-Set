a399f412ca672a6d5ee35a91404580242a01cf20
sUwUicide <56564335+sUwUicide@users.noreply.github.com>
2024-05-12 13:37:44 -0400

    Add files via upload

---
+++ Tempo Set/Tempo Set/Main.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.IO;
+using MelonLoader;
+using UnityEngine;
+using System.Collections;
+using RumbleModUI;
+
+namespace Tempo_Set
+{
+    public static class BuildInfo
+    {
+        public const string ModName = "TempoSet";
+        public const string ModVersion = "1.0.0";
+        public const string Description = "Slows Things";
+        public const string Author = "sUwUicide";
+        public const string Company = "FartsMGee";
+    }
+    public class Main : MelonMod
+    {
+        private string currentScene = "Loader";
+        private bool sceneChanged = false;
+        private bool newFile = false;
+        private string FILEPATH = @"UserData\TempoSet";
+        private string FILENAME = "Settings.txt";
+        private string[] fileContents = new string[1];
+
+        RumbleModUI.UI UI = RumbleModUIClass.UI_Obj;
+        Mod Fartsmgee = new Mod();
+        ModSetting<float> GameSpeed;
+        public override void OnLateInitializeMelon()
+        {
+        //    MelonCoroutines.Start(CheckIfFileExists(FILEPATH, FILENAME));
+            base.OnLateInitializeMelon();
+            Fartsmgee.ModName = BuildInfo.ModName;
+            Fartsmgee.ModVersion = BuildInfo.ModVersion;
+            Fartsmgee.SetFolder("TempoSetUI");
+            Fartsmgee.AddToList("Description", ModSetting.AvailableTypes.Description, "", BuildInfo.Description);
+           
+            GameSpeed = Fartsmgee.AddToList("Game Speed", 1.0f, "Changes the game engine speed where 1 is 100% gamespeed and 0.5 is 50% game speed." + Environment.NewLine +
+                "To apply values type the number in the box, press enter then save. WARNING DO NOT set your value as 0, you will have to restart your game if you do.");
+        }
+
+        public override void OnSceneWasLoaded(int buildIndex, string sceneName)
+        {
+            currentScene = sceneName;
+            sceneChanged = true;
+        }
+
+        public override void OnFixedUpdate()
+        {
+            if(Fartsmgee.GetSaveStatus())
+            {
+                Fartsmgee.ConfirmSave();
+                Time.timeScale = (float)GameSpeed.SavedValue;
+            }
+            if (UI.GetInit() && !Fartsmgee.GetUIStatus())
+                {
+                UI.AddMod(Fartsmgee);
+                MelonLogger.Msg("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+            }
+         if (currentScene != "Gym")
+         {
+             Time.timeScale = 1f;
+         }
+        }
+    }
+}
---
+++ Tempo Set/Tempo Set/Properties/AssemblyInfo.cs
@@ -0,0 +1,41 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using MelonLoader;
+using Tempo_Set;
+
+[assembly: MelonInfo(typeof(Tempo_Set.Main), Tempo_Set.BuildInfo.ModName, Tempo_Set.BuildInfo.ModVersion, Tempo_Set.BuildInfo.Author)]
+[assembly: MelonGame("Buckethead Entertainment", "RUMBLE")]
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle(Tempo_Set.BuildInfo.ModName)]
+[assembly: AssemblyDescription(Tempo_Set.BuildInfo.Description)]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany(Tempo_Set.BuildInfo.Company)]
+[assembly: AssemblyProduct(Tempo_Set.BuildInfo.ModName)]
+[assembly: AssemblyCopyright("Copyright ©  1768")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("65ef4143-59b1-4bac-93be-ff5babf7524c")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
